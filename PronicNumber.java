1238cfbf6beff5b0fc4c418f71bd528fa68c22b1
adarshpand3y <68145651+adarshpand3y@users.noreply.github.com>
2020-11-03 16:43:51 +0530

    Update PronicNumber.java

--- PronicNumber.java
+++ PronicNumber.java
@@ -1,4 +1,7 @@
 /**
+ * Program created by Parenthesis.
+ * Find me on YT/Parenthesis
+ * 
  * A pronic number is a number which is the product of
  * two consecutive integers, that is, a number of the form n(n + 1).
  * Few pronic numbers are 0, 2, 6, 12, 20, 30, 42, 56, 72, 90, 110, etc.
@@ -11,12 +14,14 @@
 import java.util.Scanner;
 class PronicNumber
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         int n;
         boolean is_pronic = false;
         System.out.println("Enter a number");
         n = sc.nextInt();
+        // Main logic
         for (int i=1; i<n; i++)
         {
             if (i*(i+1) == n)
@@ -25,6 +30,7 @@ class PronicNumber
                 break;
             }
         }
+        // Display
         if (is_pronic)
             System.out.println("The number is pronic");
         else
