bc02a784c06817b596d8d590ac11e0a67f3408de
adarshpand3y <68145651+adarshpand3y@users.noreply.github.com>
2020-11-09 20:24:54 +0530

    Add files via upload

---
+++ RemoveAllVowels.java
@@ -0,0 +1,31 @@
+/**
+ * Write a program to enter a sentence and print the sentence after removing all vowels.
+ */
+
+import java.util.Scanner;
+class RemoveAllVowels
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s, ns = "";
+        System.out.println("Enter a string");
+        s = sc.nextLine();
+        // Main calculation
+        for (int i=0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+            {
+                continue;
+            }
+            else
+            {
+                ns = ns+c;
+            }
+        }
+        // Display
+        System.out.println("The final output after removing all vowels is:-");
+        System.out.println(ns);
+    }
+}
